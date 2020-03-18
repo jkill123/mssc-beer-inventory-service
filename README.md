45345c3fbab26dc0b8c6e6cfc34392aa708c8a2f
Pinchuk Yevhen <jagernaut1233@gmail.com>
2020-03-18 14:38:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+[![CircleCI](https://circleci.com/gh/springframeworkguru/mssc-beer-inventory-service.svg?style=svg)](https://circleci.com/gh/springframeworkguru/mssc-beer-inventory-service)
+# MSSC Beer Inventory Service
---
+++ src/main/java/pinchuk/sfg/beer/inventory/service/web/mappers/BeerInventoryMapper.java
@@ -0,0 +1,16 @@
+package pinchuk.sfg.beer.inventory.service.web.mappers;
+
+import pinchuk.sfg.beer.inventory.service.domain.BeerInventory;
+import pinchuk.sfg.beer.inventory.service.web.model.BeerInventoryDto;
+import org.mapstruct.Mapper;
+
+/**
+ * Created by jt on 2019-05-31.
+ */
+@Mapper(uses = {DateMapper.class})
+public interface BeerInventoryMapper {
+
+    BeerInventory beerInventoryDtoToBeerInventory(BeerInventoryDto beerInventoryDTO);
+
+    BeerInventoryDto beerInventoryToBeerInventoryDto(BeerInventory beerInventory);
+}
