2554b55cb93cfd2858030191026fe2cd52e894bb
Pinchuk Yevhen <jagernaut1233@gmail.com>
2020-03-26 21:17:11 +0200

    added Allocation Requests

---
+++ src/main/java/pinchuk/sfg/beer/inventory/service/services/AllocationListener.java
@@ -0,0 +1,47 @@
+package pinchuk.sfg.beer.inventory.service.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+import pinchuk.sfg.beer.inventory.service.config.JmsConfig;
+import pinchuk.sfg.brewery.model.events.AllocateOrderRequest;
+import pinchuk.sfg.brewery.model.events.AllocateOrderResult;
+
+/**
+ * @author Pinchuk Yevhen
+ * @created 26/03/2020 - 20:50
+ */
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class AllocationListener {
+    private final AllocationService allocationService;
+    private final JmsTemplate jmsTemplate;
+
+    @JmsListener(destination = JmsConfig.ALLOCATE_ORDER_QUEUE)
+    public void listen(AllocateOrderRequest request){
+        AllocateOrderResult.AllocateOrderResultBuilder builder = AllocateOrderResult.builder();
+        builder.beerOrderDto(request.getBeerOrderDto());
+
+        try{
+            Boolean allocationResult = allocationService.allocateOrder(request.getBeerOrderDto());
+
+            if (allocationResult){
+                builder.pendingInventory(false);
+            } else {
+                builder.pendingInventory(true);
+            }
+
+            builder.allocationError(false);
+        } catch (Exception e){
+            log.error("Allocation failed for Order Id:" + request.getBeerOrderDto().getId());
+            builder.allocationError(true);
+        }
+
+        jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE,
+                builder.build());
+
+    }
+}
